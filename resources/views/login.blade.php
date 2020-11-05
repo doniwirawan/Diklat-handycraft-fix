8aa08ed08a9ab686c7410df15ced512396bfb249
doniwirawan <doniwirawan166@gmail.com>
2020-11-05 08:59:46 +0800

    adding login.blade.php to file

---
+++ resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<head>
+    <!-- Scripts -->
+    <script src="{{ asset('js/jquery.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/custom.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    
+</body>
+
+</html>
